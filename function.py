7491aef1317ee5ffad6300ab96890281d6497d56
Milap <milap1998@yahoo.com>
2018-10-03 22:45:27 +0200

    Finished The calculation funtion for effencisy

--- function.py
+++ function.py
@@ -83,7 +83,6 @@ def state2_Calc(targetTemp, targetEntropy, table, numberOfRows):
     return (p2,t2,h2,s2)
 
 
-
 def state1_Calc(targetTemp, table, numberOfRows):
     i = 0
  
@@ -142,9 +141,14 @@ def state3_Calc(targetPressure, table, numberOfRows):
     return (p,tI1,hI1,sI1)
 
 
+def state2_Prime_Calc(thermalEnergy, powerConsumption, state1_Enthalpy, state3_Enthalpy):
+    return ((thermalEnergy * state1_Enthalpy) - (powerConsumption * state3_Enthalpy))/(thermalEnergy - powerConsumption)
 
-
-
+def effencisyCalc(state1_Enthalpy, state2_Enthalpy, state2_Prime_Enthalpy, state3_Enthalpy):
+    COP_I = (state2_Enthalpy - state3_Enthalpy)/(state2_Enthalpy - state1_Enthalpy)
+    COP_A = (state2_Prime_Enthalpy - state3_Enthalpy)/(state2_Prime_Enthalpy - state1_Enthalpy)
+    n_Compressor = (state2_Enthalpy - state1_Enthalpy)/(state2_Prime_Enthalpy - state1_Enthalpy)
+    return (COP_I, COP_A, n_Compressor)
 
 
 saturated = HeatPumpAnalysis("D:\\reposatory\\me-program\\Files\\F001\\R410a Saturated Table.txt")
@@ -163,12 +167,11 @@ print("The result for state 2 is: " + str(Result2))
 Result3 = state3_Calc(Result2[0], saturated, numberOfRows1)
 print("The result for state 3 is: " + str(Result3))
 
+Result2_Prime = state2_Prime_Calc(10.46, 3.664, Result1[2], Result3[2])
+print("The result for state 2 prime is: " + str(Result2_Prime))
 
-# y2 = interipolation(5, 20, 8, 10, 50)       
-# print(y2)
-        
-
-
+Profermance = effencisyCalc(Result1[2], Result2[2], Result2_Prime, Result3[2])
+print("The profermance is a follows: Ideal COP = " + str(Profermance[0]) + ", Actual COP = " + str(Profermance[1]) + ", Isentropic Efficiency for Comprosser = " + str(Profermance[2]))
 
 
 
