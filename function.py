77d22d98ee0aae69b4951bece2f56233e28d6a1a
Milap <milap1998@yahoo.com>
2018-10-03 17:18:42 +0200

    Completed the step of the 2 stage introplation for the 3 stage intropl

--- function.py
+++ function.py
@@ -1,7 +1,13 @@
 from Analysis import HeatPumpAnalysis
 
+def interipolation(x1, y1, x2, x3, y3):
+    y2 = (((x2-x1)*(y3-y1))/(x3-x1))+y1
+    return y2
+
 
 def doubleInteripolation(constant1, constant2, table, numberOfRows):
+
+    arrayI1Values = []
 
     upperLimitTemp = float(constant1 + 10)
     lowerLimitTemp = float(constant1 - 10)
@@ -26,25 +32,59 @@ def doubleInteripolation(constant1, constant2, table, numberOfRows):
                 if(((float(curretRow["Entropy "])) <= constant2) and ((float(nextRow["Entropy "])) >= constant2)):
                     print("Current Row is" + str(curretRow))
                     print("nextRow Row is" + str(nextRow))
-                    
-
+                    pI1 = curretRow["Superheated Pressure"]
+                    tI1 = interipolation(float(curretRow["Entropy "]), float(curretRow["Temprature"]), constant2, float(nextRow["Entropy "]), float(nextRow["Temprature"]))
+                    hI1 = interipolation(float(curretRow["Entropy "]), float(curretRow["Enthalpy "]), constant2, float(nextRow["Entropy "]), float(nextRow["Enthalpy "]))
+                    arrayI1Values.append((pI1,tI1,hI1,constant2))
 
             curretRow = nextRow
             nextRow = table.get_row(currentRowNum + 1)
+            
+    print(arrayI1Values)
 
+    thermoPointsMin = []
+    thermoPointsMax = []
+    Tmin = constant1
+    Tmax = constant1
 
+    for i in range(0, len(arrayI1Values)):
+        thermoPoints = arrayI1Values[i]
+        t =  thermoPoints[1]
 
+        if(t <= constant1):
+            if(Tmin == constant1):
+                Tmin = t
+                thermoPointsMin.append(thermoPoints)
+
+            elif(Tmin <= t and t <= constant1):
+                Tmin = t
+                thermoPointsMin.append(thermoPoints)
+
+        
+        elif(t >= constant1):
+                if(Tmax == constant1):
+                    Tmax = t
+                    thermoPointsMax.append(thermoPoints)
+
+                elif(Tmin >= t and t >= constant1):
+                    Tmax = t
+                    thermoPointsMax.append(thermoPoints)
+
+    print(thermoPointsMin)
+    print(thermoPointsMax)
 
 
 
 superheat = HeatPumpAnalysis("D:\\reposatory\\me-program\\Files\\F001\\R410a Superheated Table.txt")
 numberOfRows = len(superheat.get_col("Superheated Pressure"))
 print("Number of row =" + str(numberOfRows))
-doubleInteripolation(28, 1.925, superheat, numberOfRows)
+doubleInteripolation(28.2, 1.925, superheat, numberOfRows)
 
 
 
-        
+
+# y2 = interipolation(5, 20, 8, 10, 50)       
+# print(y2)
         
 
 
