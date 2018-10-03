76b05045408c3bedfc1de4dae995285e4acf3d5c
Milap <milap1998@yahoo.com>
2018-10-03 12:21:50 +0200

    Current program of printing lines not working

---
+++ function.py
@@ -0,0 +1,58 @@
+from Analysis import HeatPumpAnalysis
+
+
+def doubleInteripolation(constant1, constant2, table, numberOfRows):
+    
+    curretRow = table.get_row(0)
+    nextRow = table.get_row(1)
+    currentRowNum = 0
+    
+    while (currentRowNum < numberOfRows):
+        
+        currentRowNum += 1
+
+        if(curretRow["Superheated Pressure"] is not nextRow["Superheated Pressure"]):
+            print("Current Row is" + str(curretRow))
+            print("nextRow Row is" + str(nextRow))
+            curretRow = nextRow
+            nextRow = (currentRowNum + 1)
+
+        else:
+            curretRow = nextRow
+            nextRow = (currentRowNum + 1)
+
+
+
+
+
+superheat = HeatPumpAnalysis("D:\\reposatory\\me-program\\Files\\F001\\R410a Superheated Table.txt")
+numberOfRows = len(superheat.get_col("Superheated Pressure"))
+print("Number of row =" + str(numberOfRows))
+doubleInteripolation(2, 8, superheat, numberOfRows)
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def withenBounds(targetMax, targetMin, tempTarget):
+#     if targetMax >= tempTarget and tempTarget >= targetMin:
+#         return True
+#     else:
+#         return Falses
