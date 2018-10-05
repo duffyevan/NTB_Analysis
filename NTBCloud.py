6429b9a5400ebe0613039e61977763e79fd6a296
Evan Duffy <elduffy@wpi.edu>
2018-10-05 23:42:14 +0200

    Comments on NTBCloud

--- NTBCloud.py
+++ NTBCloud.py
@@ -24,6 +24,9 @@ class NTBWebdav:
         self.client = wc.Client(login_options)
         logging.info("Logged Into NTB Webdav")
 
+    ## Back up a single file to NTB's Cloud Server
+    # @param file_path The full path to the file to be uploaded.
+    # All files are placed in /backup_files/ in the shared folder
     def backup_file(self, file_path):
         logging.info("Backing up " + file_path)
         backup_file_name = os.path.join(self.backup_location, os.path.basename(file_path))
@@ -32,15 +35,24 @@ class NTBWebdav:
         except wc.WebDavException as e:
             raise NTBCloudException(str(e))
 
+    ## Run ls on a directory or the current directory
+    # @param directory (optional) The directory to list the contents of. If not specified the current dir will be used
     def ls(self, directory=''):
         return self.client.list(directory)
 
+    ## List the files in the folder that files are backed up to. Effectively gets a list of files that have been backed up
     def list_backed_up_files(self):
         return self.ls(self.backup_location)
 
+    ## Download a single file from the backup_files folder.
+    # @param filename The name of the file in the backup_files folder to download
+    # @param download_location (optional) the location to download the file to, ./ by default
     def download_file(self, filename, download_location='./'):
         self.__download_file(os.path.join(self.backup_location, filename), download_path=download_location+filename)
 
+    ## Lover level version of download file. Downloads a file from path to path
+    # @param path The path to the file on the server to download
+    # @param download_path The path to download the file to (must include file name in path)
     def __download_file(self, path, download_path='./'):
         self.client.download(local_path=download_path, remote_path=path)
 
